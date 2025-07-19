3280267dc58419be86caeca70b159c9d248df0dc
madmonyotam <madmonyotam@gmail.com>
2025-07-19 18:07:29 +0300

    showauth  secret

--- apps/auth-service/src/index.ts
+++ apps/auth-service/src/index.ts
@@ -12,6 +12,11 @@ app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
 
+//temp
+app.get('/debug-env', (_, res) => {
+  res.send(`CASE_SECRET: ${process.env.CASE_SECRET}`);
+});
+
 app.listen(port, () => {
   console.log(`Auth service running on port ${port}`);
 });
