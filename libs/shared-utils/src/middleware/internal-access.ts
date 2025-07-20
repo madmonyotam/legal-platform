f570ffa946fe7008a30606c808ea6cba10d93cd8
madmonyotam <madmonyotam@gmail.com>
2025-07-20 14:02:33 +0300

    fix ts error for build

--- libs/shared-utils/src/middleware/internal-access.ts
+++ libs/shared-utils/src/middleware/internal-access.ts
@@ -6,7 +6,7 @@ export const enforceInternalAccess = (
   next: NextFunction
 ) => {
   const internalHeader = req.headers['x-internal-auth'] as string;
-  if (internalHeader.trim() !== process.env.INTERNAL_SECRET.trim()) {
+  if (internalHeader.trim() !== process.env.INTERNAL_SECRET?.trim()) {
     console.log('Internal header:', internalHeader);
     console.log('Expected secret:', process.env.INTERNAL_SECRET);
     return res.status(403).json({ error: 'Forbidden – external access denied' });
